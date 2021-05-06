c3fed53adcfc8d167edb9d9f871b087899573d84
Dmitrii Korshun <korshun.dimas@gmail.com>
2021-05-07 01:07:02 +0300

    create

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-APP_SECRET_KEY = 'os.getenv(APP_SECRET_KEY)'
+APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 DATABASE_CONFIG = {
     'drivername': 'postgresql+psycopg2',
-    'host': 'localhost',
-    'port': '5432',
-    'username': 'Dimas',
-    'password': 'Dima12345',
-    'database': 'chess'
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'username': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME')
 }
 
 
@@ -16,8 +16,8 @@ VK_OAUTH_CONFIG = {
     'authorize_url': 'https://oauth.vk.com/authorize',
     'access_token_url': 'https://oauth.vk.com/access_token',
 
-    'client_id': 7846692,
-    'client_secret': 'J6HWERzMqZVz6sBs1RC9',
+    'client_id': os.getenv('VK_OAUTH_CLIENT_ID'),
+    'client_secret': os.getenv('VK_OAUTH_CLIENT_SECRET'),
     'redirect_uri': 'http://localhost/authorize',
 
     'display': 'page',
